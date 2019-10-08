bf837134e49e3474171de3b364d32e8c58eb32c4
Drew Hanson <drew.o.hanson@gmail.com>
2019-10-08 10:39:14 -0600

    baselayout

--- nodequiz/src/app/shared/base-layout/base-layout.component.ts
+++ nodequiz/src/app/shared/base-layout/base-layout.component.ts
@@ -1,3 +1,12 @@
+/*
+=====================================
+  ; Title: base-layout.component.ts
+  ; Author: Drew Hanson
+  ; Date: October 8 2019
+  ; Description: base-layout.component.ts
+======================================
+*/
+
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
@@ -8,8 +17,14 @@ import { Router } from "@angular/router";
 })
 export class BaseLayoutComponent implements OnInit {
 
-  ngOnInit() {}
+  constructor( private router: Router) { }
+  ngOnInit() {
 
+  }
+logout(){
+    console.log("inside logout()!!!!");
+        this.router.navigate(['/session/login']);
+  }
 }
 
 
